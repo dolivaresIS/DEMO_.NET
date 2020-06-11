89f7992ade1483526c6a35bc5ab512dbee9d875c
David Olivares <53587080+dolivaresIS@users.noreply.github.com>
2020-06-11 17:14:24 -0400

    Cambio para la Demostración

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -12,6 +12,7 @@ namespace DemoCI
         protected void Page_Load(object sender, EventArgs e)
         {
             //Se realiza inicio
+            String nuevoStr = "valor";
 
         }
     }
